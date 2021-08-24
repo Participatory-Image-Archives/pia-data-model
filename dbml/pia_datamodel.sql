519c6e396d3565d96da73d6e68bc1563df7aa9f5
Adrian <y@thgie.ch>
2021-08-24 12:34:53 +0200

    massively refactored sql model to fit standards and incorporate learnings

--- dbml/pia_datamodel.sql
+++ dbml/pia_datamodel.sql
@@ -1,61 +1,8 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2021-08-19T13:50:49.616Z
+-- Generated at: 2021-08-24T10:29:59.164Z
 
-CREATE TABLE "Image" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "oldnr" varchar,
-  "signature" varchar,
-  "title" varchar,
-  "original_title" varchar,
-  "file_name" varchar,
-  "original_file_name" varchar,
-  "date" date[],
-  "salsah_date" varchar,
-  "sequence_number" varchar,
-  "comment" int,
-  "location" int,
-  "collection" int,
-  "verso" int,
-  "objecttype" int,
-  "model" int,
-  "format" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "References_Images" (
-  "image_a" int,
-  "image_b" int
-);
-
-CREATE TABLE "Keyword" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "label" varchar,
-  "alt_label" varchar[],
-  "description" varchar,
-  "origin" varchar,
-  "aat_id" int,
-  "aat_url" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Images_Keywords" (
-  "image_id" int,
-  "keyword_id" int
-);
-
-CREATE TABLE "Comment" (
-  "id" SERIAL PRIMARY KEY,
-  "comment" varchar[],
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Location" (
+CREATE TABLE "locations" (
   "id" SERIAL PRIMARY KEY,
   "label" varchar,
   "geonames_id" int,
@@ -67,164 +14,7 @@ CREATE TABLE "Location" (
   "updated_at" timestamp
 );
 
-CREATE TABLE "ObjectType" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "comment" varchar,
-  "aat_id" int,
-  "aat_url" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Model" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "comment" varchar,
-  "aat_id" int,
-  "aat_url" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Format" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "comment" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Person" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "name" varchar,
-  "alt_name" varchar[],
-  "title" varchar,
-  "family" varchar,
-  "gnd_id" int,
-  "gnd_url" varchar,
-  "birthplace" int,
-  "deathplace" int,
-  "birthdate" date[],
-  "deathdate" date[],
-  "salsah_birthdate" varchar,
-  "salsah_deathdate" varchar,
-  "job" varchar[],
-  "description" text,
-  "literature" varchar[],
-  "comment" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Images_Content" (
-  "image_id" int,
-  "person_id" int
-);
-
-CREATE TABLE "Images_People" (
-  "image_id" int,
-  "person_id" int
-);
-
-CREATE TABLE "Images_Copyright" (
-  "image_id" int,
-  "person_id" int
-);
-
-CREATE TABLE "Collection" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "title" varchar[],
-  "label" varchar,
-  "signature" varchar,
-  "description" text,
-  "default_image" varchar,
-  "embedded_video" varchar,
-  "date" date[],
-  "salsah_date" varchar[],
-  "literature" varchar[],
-  "comment" int,
-  "indexing" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Collections_People" (
-  "collection_id" int,
-  "person_id" int
-);
-
-CREATE TABLE "Album" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "title" varchar,
-  "label" varchar,
-  "signature" varchar,
-  "description" text,
-  "date" date[],
-  "salsah_date" varchar[],
-  "objecttype" int,
-  "collection" int,
-  "comment" int,
-  "indexing" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Albums_People" (
-  "album_id" int,
-  "person_id" int
-);
-
-CREATE TABLE "Albums_Images" (
-  "album_id" int,
-  "image_id" int
-);
-
-CREATE TABLE "Document" (
-  "id" SERIAL PRIMARY KEY,
-  "object_type" int,
-  "model" int,
-  "format" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Map" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "description" varchar,
-  "legend_id" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "MapLayer" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "map" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "MapEntry" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "description" varchar,
-  "type" int,
-  "complex_data" varchar,
-  "image_id" int,
-  "map_id" int,
-  "place_id" int,
-  "location_id" int,
-  "legend_entry_ids" int[],
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "Place" (
+CREATE TABLE "places" (
   "id" SERIAL PRIMARY KEY,
   "asv_id" varchar,
   "label" varchar,
@@ -234,7 +24,290 @@ CREATE TABLE "Place" (
   "updated_at" timestamp
 );
 
-CREATE TABLE "Legend" (
+CREATE TABLE "dates" (
+  "id" SERIAL PRIMARY KEY,
+  "date" date,
+  "date_string" varchar,
+  "type" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "keywords" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "label" varchar,
+  "description" varchar,
+  "origin" varchar,
+  "aat_id" int,
+  "aat_url" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "alt_labels" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "keyword_alt_label" (
+  "keyword_id" int,
+  "alt_label_id" int
+);
+
+CREATE TABLE "comments" (
+  "id" SERIAL PRIMARY KEY,
+  "comment" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "images" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "oldnr" varchar,
+  "signature" varchar,
+  "title" varchar,
+  "original_title" varchar,
+  "file_name" varchar,
+  "original_file_name" varchar,
+  "salsah_date" varchar,
+  "sequence_number" varchar,
+  "location" int,
+  "collection" int,
+  "verso" int,
+  "objecttype" int,
+  "model" int,
+  "format" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "image_date" (
+  "image_id" int,
+  "date_id" int
+);
+
+CREATE TABLE "image_person" (
+  "image_id" int,
+  "person_id" int
+);
+
+CREATE TABLE "image_copyright" (
+  "image_id" int,
+  "person_id" int
+);
+
+CREATE TABLE "image_image" (
+  "image_a" int,
+  "image_b" int
+);
+
+CREATE TABLE "image_keyword" (
+  "image_id" int,
+  "keyword_id" int
+);
+
+CREATE TABLE "image_comment" (
+  "image_id" int,
+  "comment_id" int
+);
+
+CREATE TABLE "object_types" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "comment" varchar,
+  "aat_id" int,
+  "aat_url" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "models" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "comment" varchar,
+  "aat_id" int,
+  "aat_url" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "formats" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "comment" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "people" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "name" varchar,
+  "title" varchar,
+  "family" varchar,
+  "gnd_id" int,
+  "gnd_url" varchar,
+  "birthplace" int,
+  "deathplace" int,
+  "description" text,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "person_date" (
+  "person_id" int,
+  "date_id" int
+);
+
+CREATE TABLE "person_comment" (
+  "person_id" int,
+  "comment_id" int
+);
+
+CREATE TABLE "jobs" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "literatures" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "person_job" (
+  "person_id" int,
+  "job_id" int
+);
+
+CREATE TABLE "person_literature" (
+  "person_id" int,
+  "literature_id" int
+);
+
+CREATE TABLE "person_alt_label" (
+  "person_id" int,
+  "alt_label_id" int
+);
+
+CREATE TABLE "collections" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "label" varchar,
+  "signature" varchar,
+  "description" text,
+  "default_image" varchar,
+  "embedded_video" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "collection_comment" (
+  "collection_id" int,
+  "comment_id" int
+);
+
+CREATE TABLE "collection_person" (
+  "collection_id" int,
+  "person_id" int
+);
+
+CREATE TABLE "collection_literature" (
+  "collection_id" int,
+  "literature_id" int
+);
+
+CREATE TABLE "collection_date" (
+  "collection_id" int,
+  "date_id" int
+);
+
+CREATE TABLE "collection_alt_label" (
+  "collection_id" int,
+  "alt_label_id" int
+);
+
+CREATE TABLE "albums" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "title" varchar,
+  "label" varchar,
+  "signature" varchar,
+  "description" text,
+  "objecttype" int,
+  "collection" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "album_date" (
+  "album_id" int,
+  "date_id" int
+);
+
+CREATE TABLE "album_person" (
+  "album_id" int,
+  "person_id" int
+);
+
+CREATE TABLE "album_image" (
+  "album_id" int,
+  "image_id" int
+);
+
+CREATE TABLE "album_comment" (
+  "album_id" int,
+  "comment_id" int
+);
+
+CREATE TABLE "documents" (
+  "id" SERIAL PRIMARY KEY,
+  "object_type" int,
+  "model" int,
+  "format" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "maps" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "description" varchar,
+  "map_key_id" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "map_layers" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "map" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "map_entries" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "description" varchar,
+  "type" int,
+  "complex_data" varchar,
+  "image_id" int,
+  "map_id" int,
+  "place_id" int,
+  "location_id" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "map_keys" (
   "id" SERIAL PRIMARY KEY,
   "label" varchar,
   "map_id" int,
@@ -242,105 +315,92 @@ CREATE TABLE "Legend" (
   "updated_at" timestamp
 );
 
-CREATE TABLE "LegendEntry" (
+CREATE TABLE "map_key_entries" (
   "id" SERIAL PRIMARY KEY,
   "label" varchar,
   "icon" varchar,
   "icon_file_name" varchar,
   "original_icon_file_name" varchar,
-  "legend_id" int,
+  "map_key_id" int,
   "created_at" timestamp,
   "updated_at" timestamp
 );
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("comment") REFERENCES "Comment" ("id");
+CREATE TABLE "map_entry_map_key_entry" (
+  "map_entry_id" int,
+  "map_key_entry_id" int
+);
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("location") REFERENCES "Location" ("id");
+ALTER TABLE "locations" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("id");
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("collection") REFERENCES "Collection" ("id");
+ALTER TABLE "places" ADD FOREIGN KEY ("location_id") REFERENCES "locations" ("id");
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("verso") REFERENCES "Image" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("location") REFERENCES "locations" ("id");
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("objecttype") REFERENCES "ObjectType" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("collection") REFERENCES "collections" ("id");
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("model") REFERENCES "Model" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("verso") REFERENCES "images" ("id");
 
-ALTER TABLE "Image" ADD FOREIGN KEY ("format") REFERENCES "Format" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("objecttype") REFERENCES "object_types" ("id");
 
-ALTER TABLE "Images_Keywords" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("model") REFERENCES "models" ("id");
 
-ALTER TABLE "Images_Keywords" ADD FOREIGN KEY ("keyword_id") REFERENCES "Keyword" ("id");
+ALTER TABLE "images" ADD FOREIGN KEY ("format") REFERENCES "formats" ("id");
 
-ALTER TABLE "Location" ADD FOREIGN KEY ("place_id") REFERENCES "Place" ("id");
+ALTER TABLE "image_keyword" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE "Person" ADD FOREIGN KEY ("birthplace") REFERENCES "Location" ("id");
+ALTER TABLE "image_keyword" ADD FOREIGN KEY ("keyword_id") REFERENCES "keywords" ("id");
 
-ALTER TABLE "Person" ADD FOREIGN KEY ("deathplace") REFERENCES "Location" ("id");
+ALTER TABLE "albums" ADD FOREIGN KEY ("objecttype") REFERENCES "object_types" ("id");
 
-ALTER TABLE "Person" ADD FOREIGN KEY ("comment") REFERENCES "Comment" ("id");
+ALTER TABLE "albums" ADD FOREIGN KEY ("collection") REFERENCES "collections" ("id");
 
-ALTER TABLE "Images_Content" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
+ALTER TABLE "album_person" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id");
 
-ALTER TABLE "Images_Content" ADD FOREIGN KEY ("person_id") REFERENCES "Person" ("id");
+ALTER TABLE "album_person" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
 
-ALTER TABLE "Images_People" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
+ALTER TABLE "album_image" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id");
 
-ALTER TABLE "Images_People" ADD FOREIGN KEY ("person_id") REFERENCES "Person" ("id");
+ALTER TABLE "album_image" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE "Images_Copyright" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
+ALTER TABLE "documents" ADD FOREIGN KEY ("object_type") REFERENCES "object_types" ("id");
 
-ALTER TABLE "Images_Copyright" ADD FOREIGN KEY ("person_id") REFERENCES "Person" ("id");
+ALTER TABLE "documents" ADD FOREIGN KEY ("model") REFERENCES "models" ("id");
 
-ALTER TABLE "Collection" ADD FOREIGN KEY ("comment") REFERENCES "Comment" ("id");
+ALTER TABLE "documents" ADD FOREIGN KEY ("format") REFERENCES "formats" ("id");
 
-ALTER TABLE "Collection" ADD FOREIGN KEY ("indexing") REFERENCES "Comment" ("id");
+ALTER TABLE "maps" ADD FOREIGN KEY ("map_key_id") REFERENCES "map_keys" ("id");
 
-ALTER TABLE "Collections_People" ADD FOREIGN KEY ("collection_id") REFERENCES "Collection" ("id");
+ALTER TABLE "map_layers" ADD FOREIGN KEY ("map") REFERENCES "maps" ("id");
 
-ALTER TABLE "Collections_People" ADD FOREIGN KEY ("person_id") REFERENCES "Person" ("id");
+ALTER TABLE "map_entries" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE "Album" ADD FOREIGN KEY ("objecttype") REFERENCES "ObjectType" ("id");
+ALTER TABLE "map_entries" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("id");
 
-ALTER TABLE "Album" ADD FOREIGN KEY ("collection") REFERENCES "Collection" ("id");
+ALTER TABLE "map_entries" ADD FOREIGN KEY ("map_id") REFERENCES "maps" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "Album" ADD FOREIGN KEY ("comment") REFERENCES "Comment" ("id");
+ALTER TABLE "map_keys" ADD FOREIGN KEY ("map_id") REFERENCES "maps" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "Album" ADD FOREIGN KEY ("indexing") REFERENCES "Comment" ("id");
+ALTER TABLE "map_key_entries" ADD FOREIGN KEY ("map_key_id") REFERENCES "map_keys" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "Albums_People" ADD FOREIGN KEY ("album_id") REFERENCES "Album" ("id");
+COMMENT ON TABLE "places" IS 'A Place is an unprecise concept related to geography.';
 
-ALTER TABLE "Albums_People" ADD FOREIGN KEY ("person_id") REFERENCES "Person" ("id");
+COMMENT ON TABLE "dates" IS 'Type can be either:
+  1: date, a simple date with unclear definition (often it designates)
+  2: start_date, in case of a daterange
+  3: end_date, in case of a daterange
+  4: birth_date, for a person
+  5: death_date, for a person';
 
-ALTER TABLE "Albums_Images" ADD FOREIGN KEY ("album_id") REFERENCES "Album" ("id");
+COMMENT ON TABLE "images" IS 'An image can have
+- dates
+- people, displayed or as copyright
+- references to other images
+- keywords
+- comments';
 
-ALTER TABLE "Albums_Images" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
-
-ALTER TABLE "Document" ADD FOREIGN KEY ("object_type") REFERENCES "ObjectType" ("id");
-
-ALTER TABLE "Document" ADD FOREIGN KEY ("model") REFERENCES "Model" ("id");
-
-ALTER TABLE "Document" ADD FOREIGN KEY ("format") REFERENCES "Format" ("id");
-
-ALTER TABLE "Map" ADD FOREIGN KEY ("legend_id") REFERENCES "Legend" ("id");
-
-ALTER TABLE "MapLayer" ADD FOREIGN KEY ("map") REFERENCES "Map" ("id");
-
-ALTER TABLE "MapEntry" ADD FOREIGN KEY ("image_id") REFERENCES "Image" ("id");
-
-ALTER TABLE "MapEntry" ADD FOREIGN KEY ("place_id") REFERENCES "Place" ("id");
-
-ALTER TABLE "MapEntry" ADD FOREIGN KEY ("map_id") REFERENCES "Map" ("id") ON DELETE CASCADE;
-
-ALTER TABLE "Place" ADD FOREIGN KEY ("location_id") REFERENCES "Location" ("id");
-
-ALTER TABLE "Legend" ADD FOREIGN KEY ("map_id") REFERENCES "Map" ("id") ON DELETE CASCADE;
-
-ALTER TABLE "LegendEntry" ADD FOREIGN KEY ("legend_id") REFERENCES "Legend" ("id") ON DELETE CASCADE;
-
-COMMENT ON TABLE "MapEntry" IS 'Type can be either:
+COMMENT ON TABLE "map_entries" IS 'Type can be either:
   0: place, refering to a place and getting coordinates from there
   1: precise, which is a marker placed on the map by hand; leading to the creation of a location
   2: complex, which is mutiple markers, a shape or line or an image; stores the information in complex_data
   3: image, image placed by hand; stores information in complex_data';
-
-COMMENT ON TABLE "Place" IS 'A Place is an unprecise concept related to geography.';
