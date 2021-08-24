3fab2da7e0661bf588dcbe4b1abf34d3b5ae9e70
Adrian <y@thgie.ch>
2021-08-24 13:44:26 +0200

    proper definition of relations

--- dbml/pia_datamodel.sql
+++ dbml/pia_datamodel.sql
@@ -1,6 +1,6 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2021-08-24T10:29:59.164Z
+-- Generated at: 2021-08-24T11:44:07.678Z
 
 CREATE TABLE "locations" (
   "id" SERIAL PRIMARY KEY,
@@ -60,6 +60,69 @@ CREATE TABLE "keyword_alt_label" (
 CREATE TABLE "comments" (
   "id" SERIAL PRIMARY KEY,
   "comment" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "documents" (
+  "id" SERIAL PRIMARY KEY,
+  "object_type" int,
+  "model" int,
+  "format" int,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "people" (
+  "id" SERIAL PRIMARY KEY,
+  "salsah_id" int,
+  "name" varchar,
+  "title" varchar,
+  "family" varchar,
+  "gnd_id" int,
+  "gnd_url" varchar,
+  "birthplace" int,
+  "deathplace" int,
+  "description" text,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "person_date" (
+  "person_id" int,
+  "date_id" int
+);
+
+CREATE TABLE "person_comment" (
+  "person_id" int,
+  "comment_id" int
+);
+
+CREATE TABLE "person_job" (
+  "person_id" int,
+  "job_id" int
+);
+
+CREATE TABLE "person_literature" (
+  "person_id" int,
+  "literature_id" int
+);
+
+CREATE TABLE "person_alt_label" (
+  "person_id" int,
+  "alt_label_id" int
+);
+
+CREATE TABLE "jobs" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+CREATE TABLE "literatures" (
+  "id" SERIAL PRIMARY KEY,
+  "label" varchar,
   "created_at" timestamp,
   "updated_at" timestamp
 );
@@ -143,60 +206,6 @@ CREATE TABLE "formats" (
   "updated_at" timestamp
 );
 
-CREATE TABLE "people" (
-  "id" SERIAL PRIMARY KEY,
-  "salsah_id" int,
-  "name" varchar,
-  "title" varchar,
-  "family" varchar,
-  "gnd_id" int,
-  "gnd_url" varchar,
-  "birthplace" int,
-  "deathplace" int,
-  "description" text,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "person_date" (
-  "person_id" int,
-  "date_id" int
-);
-
-CREATE TABLE "person_comment" (
-  "person_id" int,
-  "comment_id" int
-);
-
-CREATE TABLE "jobs" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "literatures" (
-  "id" SERIAL PRIMARY KEY,
-  "label" varchar,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
-CREATE TABLE "person_job" (
-  "person_id" int,
-  "job_id" int
-);
-
-CREATE TABLE "person_literature" (
-  "person_id" int,
-  "literature_id" int
-);
-
-CREATE TABLE "person_alt_label" (
-  "person_id" int,
-  "alt_label_id" int
-);
-
 CREATE TABLE "collections" (
   "id" SERIAL PRIMARY KEY,
   "salsah_id" int,
@@ -267,15 +276,6 @@ CREATE TABLE "album_comment" (
   "comment_id" int
 );
 
-CREATE TABLE "documents" (
-  "id" SERIAL PRIMARY KEY,
-  "object_type" int,
-  "model" int,
-  "format" int,
-  "created_at" timestamp,
-  "updated_at" timestamp
-);
-
 CREATE TABLE "maps" (
   "id" SERIAL PRIMARY KEY,
   "label" varchar,
@@ -335,6 +335,36 @@ ALTER TABLE "locations" ADD FOREIGN KEY ("place_id") REFERENCES "places" ("id");
 
 ALTER TABLE "places" ADD FOREIGN KEY ("location_id") REFERENCES "locations" ("id");
 
+ALTER TABLE "keyword_alt_label" ADD FOREIGN KEY ("keyword_id") REFERENCES "keywords" ("id");
+
+ALTER TABLE "keyword_alt_label" ADD FOREIGN KEY ("alt_label_id") REFERENCES "alt_labels" ("id");
+
+ALTER TABLE "documents" ADD FOREIGN KEY ("object_type") REFERENCES "object_types" ("id");
+
+ALTER TABLE "documents" ADD FOREIGN KEY ("model") REFERENCES "models" ("id");
+
+ALTER TABLE "documents" ADD FOREIGN KEY ("format") REFERENCES "formats" ("id");
+
+ALTER TABLE "person_date" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "person_date" ADD FOREIGN KEY ("date_id") REFERENCES "dates" ("id");
+
+ALTER TABLE "person_comment" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "person_comment" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("id");
+
+ALTER TABLE "person_job" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "person_job" ADD FOREIGN KEY ("job_id") REFERENCES "jobs" ("id");
+
+ALTER TABLE "person_literature" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "person_literature" ADD FOREIGN KEY ("literature_id") REFERENCES "literatures" ("id");
+
+ALTER TABLE "person_alt_label" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "person_alt_label" ADD FOREIGN KEY ("alt_label_id") REFERENCES "alt_labels" ("id");
+
 ALTER TABLE "images" ADD FOREIGN KEY ("location") REFERENCES "locations" ("id");
 
 ALTER TABLE "images" ADD FOREIGN KEY ("collection") REFERENCES "collections" ("id");
@@ -347,13 +377,57 @@ ALTER TABLE "images" ADD FOREIGN KEY ("model") REFERENCES "models" ("id");
 
 ALTER TABLE "images" ADD FOREIGN KEY ("format") REFERENCES "formats" ("id");
 
+ALTER TABLE "image_date" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
+
+ALTER TABLE "image_date" ADD FOREIGN KEY ("date_id") REFERENCES "dates" ("id");
+
+ALTER TABLE "image_person" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
+
+ALTER TABLE "image_person" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "image_copyright" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
+
+ALTER TABLE "image_copyright" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "image_image" ADD FOREIGN KEY ("image_a") REFERENCES "images" ("id");
+
+ALTER TABLE "image_image" ADD FOREIGN KEY ("image_b") REFERENCES "images" ("id");
+
 ALTER TABLE "image_keyword" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
 ALTER TABLE "image_keyword" ADD FOREIGN KEY ("keyword_id") REFERENCES "keywords" ("id");
 
+ALTER TABLE "image_comment" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
+
+ALTER TABLE "image_comment" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("id");
+
+ALTER TABLE "collection_comment" ADD FOREIGN KEY ("collection_id") REFERENCES "collections" ("id");
+
+ALTER TABLE "collection_comment" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("id");
+
+ALTER TABLE "collection_person" ADD FOREIGN KEY ("collection_id") REFERENCES "collections" ("id");
+
+ALTER TABLE "collection_person" ADD FOREIGN KEY ("person_id") REFERENCES "people" ("id");
+
+ALTER TABLE "collection_literature" ADD FOREIGN KEY ("collection_id") REFERENCES "collections" ("id");
+
+ALTER TABLE "collection_literature" ADD FOREIGN KEY ("literature_id") REFERENCES "literatures" ("id");
+
+ALTER TABLE "collection_date" ADD FOREIGN KEY ("collection_id") REFERENCES "collections" ("id");
+
+ALTER TABLE "collection_date" ADD FOREIGN KEY ("date_id") REFERENCES "dates" ("id");
+
+ALTER TABLE "collection_alt_label" ADD FOREIGN KEY ("collection_id") REFERENCES "collections" ("id");
+
+ALTER TABLE "collection_alt_label" ADD FOREIGN KEY ("alt_label_id") REFERENCES "alt_labels" ("id");
+
 ALTER TABLE "albums" ADD FOREIGN KEY ("objecttype") REFERENCES "object_types" ("id");
 
 ALTER TABLE "albums" ADD FOREIGN KEY ("collection") REFERENCES "collections" ("id");
+
+ALTER TABLE "album_date" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id");
+
+ALTER TABLE "album_date" ADD FOREIGN KEY ("date_id") REFERENCES "dates" ("id");
 
 ALTER TABLE "album_person" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id");
 
@@ -363,11 +437,9 @@ ALTER TABLE "album_image" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id"
 
 ALTER TABLE "album_image" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
 
-ALTER TABLE "documents" ADD FOREIGN KEY ("object_type") REFERENCES "object_types" ("id");
+ALTER TABLE "album_comment" ADD FOREIGN KEY ("album_id") REFERENCES "albums" ("id");
 
-ALTER TABLE "documents" ADD FOREIGN KEY ("model") REFERENCES "models" ("id");
-
-ALTER TABLE "documents" ADD FOREIGN KEY ("format") REFERENCES "formats" ("id");
+ALTER TABLE "album_comment" ADD FOREIGN KEY ("comment_id") REFERENCES "comments" ("id");
 
 ALTER TABLE "maps" ADD FOREIGN KEY ("map_key_id") REFERENCES "map_keys" ("id");
 
@@ -383,7 +455,13 @@ ALTER TABLE "map_keys" ADD FOREIGN KEY ("map_id") REFERENCES "maps" ("id") ON DE
 
 ALTER TABLE "map_key_entries" ADD FOREIGN KEY ("map_key_id") REFERENCES "map_keys" ("id") ON DELETE CASCADE;
 
-COMMENT ON TABLE "places" IS 'A Place is an unprecise concept related to geography.';
+ALTER TABLE "map_entry_map_key_entry" ADD FOREIGN KEY ("map_entry_id") REFERENCES "map_entries" ("id");
+
+ALTER TABLE "map_entry_map_key_entry" ADD FOREIGN KEY ("map_key_entry_id") REFERENCES "map_key_entries" ("id");
+
+COMMENT ON TABLE "locations" IS '“Location” is defined as “the specific area where a place is situated.”';
+
+COMMENT ON TABLE "places" IS 'See locations.';
 
 COMMENT ON TABLE "dates" IS 'Type can be either:
   1: date, a simple date with unclear definition (often it designates)
